7fbd2f87580efc42d182a95699407a9e29f48c0b
bhuiyanluke <36482524+bhuiyanluke@users.noreply.github.com>
2018-11-10 16:11:44 +0200

    data
    data

---
+++ data/create_learning2014.R
@@ -0,0 +1,14 @@
+# Rabbil Bhuiyan
+# 10.11.2018
+# Data wrangling practice 
+# read the data
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+# dimension of the data
+dim(lrn14)
+
+#structure of the data
+str(lrn14)
+
+#Output of the codes
+#183 observations and 60 variables were found 
